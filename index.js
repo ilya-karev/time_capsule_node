01fb7e479e28b47f8d4e99df481431fc27746354
Ilia Karev <ilya.karev1000@gmail.com>
2022-08-28 22:19:00 +0200

    setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to mongodb atlas')
+  })
+  .catch(error => {
+    console.log(`Something wrong happen`, error)
+  })
+
+app.listen(PORT, () => {
+  console.log(`Server started at PORT ${PORT}`)
+})
